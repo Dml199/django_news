8ad44649a7cc5f0dbf62cf0a85ea2d75615443ff
Dmitry <dmitrijladov396@gmail.com>
2025-06-18 22:26:55 +0300

    second

--- news/news_/models.py
+++ news/news_/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 class News(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    pub_date = models.DateField(auto_now_add=True)
+    content = models.TextField()  # поле для текста новости
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # связь с автором
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+    
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
---
+++ news/news_/templatetags/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from django import forms
+from ..models import News
+
+class NewsFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Название')
+    author__name = django_filters.CharFilter(field_name='author__name', lookup_expr='icontains', label='Автор')
+    created_at__gt = django_filters.DateFilter(
+        field_name='created_at',
+        lookup_expr='gt',
+        label='Позже даты',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+
+    class Meta:
+        model = News
+        fields = ['title', 'author__name', 'created_at__gt']
